5a8ad9e809dca5bb74776f4f183e5d9a6df52e4e
Armando Dollia <dollia.armando@gmail.com>
2015-02-24 16:44:37 -0600

    start chapter 5

--- Chapter 5/src/NumberAverage_5_1.java
+++ Chapter 5/src/NumberAverage_5_1.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -10,20 +11,43 @@ import java.util.Scanner;
 public class NumberAverage_5_1 {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		Integer number, length, positives, negatives;
 		
+		Integer positives = 0, negatives = 0;
+		Double average, total = 0.0, number;
 		
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter an integer, the input ends if it is 0: ");
-		while(input.hasNext()){
-		number = input.nextInt();
-		list.add(number);
+		
+		number = input.nextDouble();
+		
+		while(number != 0){
+			total += number;
+			
+			if(number > 0.0){
+				positives++;
+			}
+			else if(number < 0.0){
+				negatives++;
+			}
+			
+			number = input.nextDouble();
 		}
 		
+		average = (double) (total / (negatives + positives));
 		
+		if(number == 0 && positives+negatives == 0){
+			System.out.println("No numbers are entered except 0");
+		}
 		
-
+		else{
+			System.out.println("The number of positives is " + positives);
+			System.out.println("The number of negatives is " + negatives);
+			System.out.println("The total is " + total);
+			System.out.println("The average is " + average);
+		}
+		
+		input.close();
+		
 	}
 
 }
---
+++ Chapter 5/src/PoundKilogramConverter_5_5.java
@@ -0,0 +1,14 @@
+
+public class PoundKilogramConverter_5_5 {
+
+	public static void main(String[] args) {
+		Double kilo, pound;
+		String kp, pk;
+		
+		for(int i = 1; i < 200; i+=2){
+			
+		}
+
+	}
+
+}
